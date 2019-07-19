b5d11e2fc1f4ceeec8a70f58eea0776a8b203815
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-19 16:25:08 -0600

    After refactoring to hooks, the app was not working so an npm update of all the depencies fixed it

--- src/index.js
+++ src/index.js
@@ -1,46 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
 import Loader from './Loader.js'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: null,
-      errorMessage: ''
-    }
-  }
+// componentDidMount() {
+//   window.navigator.geolocation.getCurrentPosition(position => this.setState({
+//     lat: position.coords.latitude
+//   }),
+//     err => this.setState({
+//       errorMessage: err.message
+//     })
+//   )
+// }
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(position => this.setState({ 
-        lat: position.coords.latitude
-      }),
-      err => this.setState({ 
-        errorMessage: err.message
-      })
+// renderContent() {
+//   if (this.state.errorMessage && !this.state.lat) {
+//     return <div>Error: {this.state.errorMessage}</div>
+//   }
+
+//   if (!this.state.errorMessage && this.state.lat) {
+//     return <SeasonDisplay lat={this.state.lat} />
+//   }
+
+//   return <Loader />
+// }
+
+const App = () => {
+  const [lat, setLat] = useState(null)
+  const [error, setErr] = useState('')
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => setLat(position.coords.latitude),
+      err => setErr(err.message)
     )
+  }, [])
+
+  let content;
+  if (error) {
+    content = <div> Error: {error} </div>
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />
+  } else {
+    content = <Loader />
   }
 
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
-    }
-
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat}/>
-    }
-
-    return <Loader />
-  }
-
-  render () {
-    return (
-    <div className="border red">
-      {this.renderContent()}
-    </div>
-    )
-  }
+  return <div className="border red"> {content} </div>
 }
 
-ReactDOM.render(<App />,document.querySelector('#root'))
+
+ReactDOM.render(<App />, document.querySelector('#root'))

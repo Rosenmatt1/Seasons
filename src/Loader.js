2a65918e19ba3af4c7c27f5ae75227d6defa0365
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-19 15:06:06 -0600

    project before converting to hooks

--- src/Loader.js
+++ src/Loader.js
@@ -2,17 +2,10 @@ import React from 'react'
 
 const Loader = (props) => {
   
-
   return (
- 
     <div className="ui active dimmer">
       <div className="ui text loader">{props.message ||'Loading'}</div>
     </div>
-
-    // Loader.defaultProps {
-    //   message: 'Loading...'
-    // }
-  
  
   )
 }
--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -24,7 +24,6 @@ const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth())
   const {text, iconName} = seasonConfig[season]
 
-
   return (
     <div className={`season-display ${season}`}>
       <i className={`icon-left massive ${iconName} icon`} />
